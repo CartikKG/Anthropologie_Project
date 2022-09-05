7246d835631f09922e67f2c11366d093063afbf9
Anchala-Gurumohan <anchalagurumohan@gmail.com>
2022-09-05 20:24:34 +0530

    complete data

---
+++ mohanData/beauty&wellnessShopByCategory/topRated.js
@@ -0,0 +1,89 @@
+var topRated = [
+    {
+        name: "Ellis Brooklyn Eau De Parfum",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/62553177_020_a?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/62553177_020_b10?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_3: "https://images.urbndata.com/is/image/Anthropologie/62553177_020_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "84.00",
+        strikedPrice:"105.00"
+    },
+    {
+        name: "Nest Fragrances Eau De Parfum",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/49216096_042_b10?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/49216096_042_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_3: "https://images.urbndata.com/is/image/Anthropologie/49216096_042_b14?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "65.60",
+        strikedPrice:"82.00"
+    },
+    {
+        name: "Tabu The Lubricant",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/79252979_080_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "31.20",
+        strikedPrice:"39.00"
+    },
+    {
+        name: "Tocca Eau De Parfum",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/37394095_237_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "64.00",
+        strikedPrice:"80.00"
+    },
+    {
+        name: "Sunday Riley Good Genes All-In-One Lactic Acid Treatment, 1.7 oz.",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/41540519_010_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/41540519_010_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "97.60",
+        strikedPrice:"122.00"
+    },
+    {
+        name: "Mason Pearson Handy Brush",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/37307238_021_b3?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/37307238_021_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_3: "https://images.urbndata.com/is/image/Anthropologie/37307238_021_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "140.00",
+        strikedPrice:"175.00"
+    },
+    {
+        name: "Kai Rose Eau De Parfum",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/44830974_010_b14?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/44830974_010_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "64.00",
+        strikedPrice:"80.00"
+    },
+    {
+        name: "Ellis Brooklyn Eau De Parfum",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/62553177_020_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/62553177_020_a?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_3: "https://images.urbndata.com/is/image/Anthropologie/62553177_020_b10?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "84.00",
+        strikedPrice: "105.00"
+    },
+    {
+        name: "Lapcos Variety Mask + Exfoliating Pad Set",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/44499010_000_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "20.80",
+        strikedPrice:"26.00"
+    },
+    {
+        name: "Herbivore Botanicals Coco Rose Body Polish",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/44520971_066_b15?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/44520971_066_b11?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_3: "https://images.urbndata.com/is/image/Anthropologie/44520971_066_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "30.40",
+        strikedPrice:"38.00"
+    },
+    {
+        name: "Nest Fragrances Eau De Parfum",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/49216096_042_b14?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/49216096_042_b10?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_3: "https://images.urbndata.com/is/image/Anthropologie/49216096_042_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "65.60",
+        strikedPrice:"82.00"
+    },
+    {
+        name: "Baudelaire Small Wool Sponge",
+        image_1: "https://images.urbndata.com/is/image/Anthropologie/38368379_025_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        image_2: "https://images.urbndata.com/is/image/Anthropologie/38368379_025_b?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+        price: "18.40",
+        strikedPrice:"23.00"
+    },
+]
