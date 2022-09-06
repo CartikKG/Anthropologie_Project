d26226ba0f70dcccb4adcbb715de14bac4b5857e
shabbir yahya <shabbirkatlariwala@gmail.com>
2022-09-06 15:41:18 +0530

    SignIn_SignUP only Css other js work and page combining works are remaiing

---
+++ SignUp__SignIn/SINGUP_SIGNIN/Singup_Signin.js
@@ -0,0 +1,15 @@
+//define the function
+
+// function alertsomething() {
+//     alert("Hey, This is an alert inside that function");
+// }
+
+// //function works when button clicks
+// $("#btd").click(function() {
+//     alertsomething();
+// })
+
+// //function automatically when page loads
+// $(document).ready(function() {
+//     alertsomething();
+// })
