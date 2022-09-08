48e4ff98beb245976b74f0bd2f6d7d83c0e85a8e
shabbir yahya <shabbirkatlariwala@gmail.com>
2022-09-09 00:16:57 +0530

    SignIn_SignUp in master

--- SignUp__SignIn/Sign_In_By_Mobile/Sign_In_By_Mobile.js
+++ SignUp__SignIn/Sign_In_By_Mobile/Sign_In_By_Mobile.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-  $("#parent1").modal("show");
-});
-
-$(document).ready(function () {
-  $(".modal_close").bind("click", function () {
-    window.location.href = "../../ProductsPage/womensClothing.html";
-  });
-});
+$(document).ready(function() {
+    $('#parent1').modal('show');
+})
