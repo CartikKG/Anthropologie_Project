eb28102322da17adf097f8391e5ed64b30bbf0dd
Pankaj_Mehra <meenakshi9991833@gmail.com>
2022-09-08 07:46:59 +0530

    description view page created

--- productsReviewPage/productsReviewPage.js
+++ productsReviewPage/productsReviewPage.js
@@ -1,13 +1,26 @@
-let bigimage = document.getElementById("kartik");
+var getname = localStorage.getItem("itemName");
+var getimage1 = localStorage.getItem("image_1");
+var getimage2 = localStorage.getItem("image_2");
+var getimage3 = localStorage.getItem("image_3");
+var getprice = localStorage.getItem("price");
 
-let tempview = JSON.parse(localStorage.getItem("forcheckitemview"));
-// console.log(tempview);
-// console.log("kartik");
-// console.log("kartik");
-// console.log("kartik");
 
-// bigimage = ;
-bigimage.setAttribute("src", tempview[0].image_1);
-// bigimage.innerHTML = tempview[0].image_1;
-// bigimage.innerText = tempview[0].image_1;
-// console.log(tempview[0].image_1);
+var itemName = document.createElement("h1");
+var image1 = document.createElement("img");
+var image2 = document.createElement("img");
+var image3 = document.createElement("img");
+var price = document.createElement("span");
+
+itemName.setAttribute("id", "nameOfTheProduct");
+
+console.log(getname);
+console.log(getprice);
+itemName.innerText = getname;
+image1.setAttribute("src", getimage1);
+image2.setAttribute("src", getimage2);
+image3.setAttribute("src", getimage3);
+price.innerText = "$" + getprice;
+document.querySelector(".leftSideImageDiv").append(image1, image2, image3);
+document.querySelector(".mainImageDiv").append(image1);
+document.getElementById("productNameBox").append(itemName);
+document.getElementById("productCost").append(price);
